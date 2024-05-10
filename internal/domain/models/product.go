d40fc78a3dd517da3afb7ce25b8eedc35dc21c0f
asadbek <tozhimukhammedov.aa@phystech.edu>
2024-05-10 15:58:31 +0300

    better product model

--- internal/domain/models/product.go
+++ internal/domain/models/product.go
@@ -1,8 +1,9 @@
 package models
 
 type Product struct {
-	ID       uint64 `json:"id" db:"id"`
-	Name     string `json:"name" db:"name" binding:"required"`
-	Price    uint64 `json:"price" db:"price" binding:"required"`
-	Quantity uint64 `json:"quantity" db:"quantity" binding:"required"`
+	ID        uint64 `json:"id" db:"id"`
+	Name      string `json:"name" db:"name" binding:"required"`
+	Price     uint64 `json:"price" db:"price" binding:"required"`
+	Quantity  uint64 `json:"quantity" db:"quantity" binding:"required"`
+	OwnerName string `json:"owner_name" db:"owner_name" binding:"required"`
 }
