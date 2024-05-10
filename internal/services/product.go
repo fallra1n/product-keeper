c4298f65f59710b2f1281caddc4a3d03b2ed635b
asadbek <tozhimukhammedov.aa@phystech.edu>
2024-05-10 15:57:52 +0300

    added create product in service layer

--- internal/services/product.go
+++ internal/services/product.go
@@ -1,8 +1,15 @@
 package services
 
-import "github.com/fallra1n/product-service/internal/storage"
+import (
+	"github.com/fallra1n/product-service/internal/domain/models"
+	"github.com/fallra1n/product-service/internal/storage"
+)
 
 type Product interface {
+	CreateProduct(product models.Product) (uint64, error)
+	GetProductByID(id uint64) (models.Product, error)
+	UpdateProductByID(newProduct models.Product) (models.Product, error)
+	DeleteProductByID(id uint64) error
 }
 
 type productService struct {
@@ -11,4 +18,20 @@ type productService struct {
 
 func NewProductService(storage storage.Storage) Product {
 	return &productService{storage}
+}
+
+func (s *productService) CreateProduct(product models.Product) (uint64, error) {
+	return s.storage.CreateProduct(product)
+}
+
+func (s *productService) GetProductByID(id uint64) (models.Product, error) {
+	return models.Product{}, nil
+}
+
+func (s *productService) UpdateProductByID(newProduct models.Product) (models.Product, error) {
+	return models.Product{}, nil
+}
+
+func (s *productService) DeleteProductByID(id uint64) error {
+	return nil
 }
