5804a4ef15e5f3c246b9d0c6fbc3a216bc2d4c09
Asadbek Tozhimukhammedov <tozhimukhammedov.aa@yandex.ru>
2024-08-05 18:17:27 +0300

    migrations: migrations files has been formatted

--- migrations/000001_init.up.sql
+++ migrations/000001_init.up.sql
@@ -1,16 +1,16 @@
 CREATE TABLE IF NOT EXISTS auth$users
-		(
-			name VARCHAR(255) NOT NULL UNIQUE,
-		    password VARCHAR(255) NOT NULL
-		);
+  (
+     name     VARCHAR(255) NOT NULL UNIQUE,
+     password VARCHAR(255) NOT NULL
+  );
 
 CREATE TABLE IF NOT EXISTS products
-		(
-		    id SERIAL PRIMARY KEY,
-		    name VARCHAR(255) NOT NULL,
-		    price INT NOT NULL,
-		    quantity INT NOT NULL,
-		    owner_name VARCHAR(255) NOT NULL,
-		    created_at TIMESTAMP NOT NULL,
-		    FOREIGN KEY (owner_name) REFERENCES auth$users(name)
-		);
+  (
+     id         SERIAL PRIMARY KEY,
+     name       VARCHAR(255) NOT NULL,
+     price      INT NOT NULL,
+     quantity   INT NOT NULL,
+     owner_name VARCHAR(255) NOT NULL,
+     created_at TIMESTAMP NOT NULL,
+     FOREIGN KEY (owner_name) REFERENCES auth$users(name)
+  ); 
