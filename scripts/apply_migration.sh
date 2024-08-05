87c8d3eed76355ac7e992b4be082179b1f184e30
Asadbek Tozhimukhammedov <tozhimukhammedov.aa@yandex.ru>
2024-08-05 19:54:27 +0300

    migration: added retry when applying migrations

--- scripts/apply_migration.sh
+++ scripts/apply_migration.sh
@@ -4,11 +4,31 @@ CUR_DIR=$(dirname $0)
 
 export DB_NAME=postgres
 export DB_PASSWORD=pass
+export DB_PORT=${DB_PORT:-5432}
 
 if [ -z "$1" ]
-  then
-    echo "Укажите номер миграции"
-    exit 1
+then
+  echo "enter the number of migration"
+  exit 1
 fi
 
-migrate -path ${CUR_DIR?}/../migrations -database "postgres://${DB_NAME?}:${DB_PASSWORD?}@localhost:5432/${DB_NAME?}?sslmode=disable" up $1
+MAX_RETRIES=5
+RETRY_COUNT=0
+SUCCESS=0
+
+while [ ${RETRY_COUNT?} -lt ${MAX_RETRIES?} ]; do
+  migrate -path ${CUR_DIR?}/../migrations -database "postgres://${DB_NAME?}:${DB_PASSWORD?}@localhost:${DB_PORT?}/${DB_NAME?}?sslmode=disable" up $1
+  
+  if [ $? -eq 0 ]; then
+    SUCCESS=1
+    break
+  else
+    sleep 20
+    RETRY_COUNT=$((RETRY_COUNT+1))
+  fi
+done
+
+if [ $SUCCESS -ne 1 ]; then
+  echo "failed to apply migration"
+  exit 1
+fi
