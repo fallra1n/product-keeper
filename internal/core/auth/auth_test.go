faf418db17c317dc100996ecbb87a0d82dfa6b17
Asadbek Tozhimukhammedov <tozhimukhammedov.aa@yandex.ru>
2024-08-13 19:16:23 +0500

    auth: tests for method LoginUser have been added

--- internal/core/auth/auth_test.go
+++ internal/core/auth/auth_test.go
@@ -32,6 +32,7 @@ func (s *RunAuthSuite) TestCreateUser() {
 	type fields struct {
 		tx       *sqlx.Tx
 		crypto   *mockshared.MockCrypto
+		jwt      *mockshared.MockJwt
 		authRepo *mockauth.MockAuthRepo
 	}
 
@@ -88,6 +89,7 @@ func (s *RunAuthSuite) TestCreateUser() {
 			f := fields{
 				tx:       &sqlx.Tx{},
 				crypto:   mockshared.NewMockCrypto(ctrl),
+				jwt:      mockshared.NewMockJwt(ctrl),
 				authRepo: mockauth.NewMockAuthRepo(ctrl),
 			}
 			if row.prepare != nil {
@@ -98,10 +100,112 @@ func (s *RunAuthSuite) TestCreateUser() {
 				s.log,
 				f.authRepo,
 				f.crypto,
+				f.jwt,
 			)
 
 			err := service.CreateUser(f.tx, row.args)
 			s.Equal(row.err, err)
+		})
+	}
+}
+
+func (s *RunAuthSuite) TestLoginUser() {
+	type fields struct {
+		tx       *sqlx.Tx
+		crypto   *mockshared.MockCrypto
+		jwt      *mockshared.MockJwt
+		authRepo *mockauth.MockAuthRepo
+	}
+
+	var (
+		mockUser           = auth.User{Name: "test name", Password: "test pass"}
+		mockHashedPassword = "test hashed pass"
+		mockToken          = "test jwt token"
+	)
+
+	testList := []struct {
+		name     string
+		prepare  func(f *fields)
+		args     auth.User
+		expected string
+		err      error
+	}{
+		{
+			name: "successful launch",
+			prepare: func(f *fields) {
+				gomock.InOrder(
+					f.authRepo.EXPECT().FindPassword(f.tx, mockUser.Name).Return(mockHashedPassword, nil),
+					f.crypto.EXPECT().CompareHashAndPassword(mockHashedPassword, mockUser.Password).Return(nil),
+					f.jwt.EXPECT().GenerateToken(mockUser.Name).Return(mockToken, nil),
+				)
+			},
+			args:     mockUser,
+			expected: mockToken,
+			err:      nil,
+		},
+		{
+			name: "user not found",
+			prepare: func(f *fields) {
+				gomock.InOrder(
+					f.authRepo.EXPECT().FindPassword(f.tx, mockUser.Name).Return("", auth.ErrUserNotFound),
+				)
+			},
+			args:     mockUser,
+			expected: "",
+			err:      auth.ErrUserNotFound,
+		},
+		{
+			name: "incorrect password",
+			prepare: func(f *fields) {
+				gomock.InOrder(
+					f.authRepo.EXPECT().FindPassword(f.tx, mockUser.Name).Return(mockHashedPassword, nil),
+					f.crypto.EXPECT().CompareHashAndPassword(mockHashedPassword, mockUser.Password).Return(shared.ErrNoData),
+				)
+			},
+			args:     mockUser,
+			expected: "",
+			err:      auth.ErrIncorrectPassword,
+		},
+		{
+			name: "failed to generate token",
+			prepare: func(f *fields) {
+				gomock.InOrder(
+					f.authRepo.EXPECT().FindPassword(f.tx, mockUser.Name).Return(mockHashedPassword, nil),
+					f.crypto.EXPECT().CompareHashAndPassword(mockHashedPassword, mockUser.Password).Return(nil),
+					f.jwt.EXPECT().GenerateToken(mockUser.Name).Return("", shared.ErrNoData),
+				)
+			},
+			args:     mockUser,
+			expected: "",
+			err:      shared.ErrInternal,
+		},
+	}
+
+	for _, row := range testList {
+		s.Run(row.name, func() {
+			ctrl := gomock.NewController(s.T())
+			defer ctrl.Finish()
+
+			f := fields{
+				tx:       &sqlx.Tx{},
+				crypto:   mockshared.NewMockCrypto(ctrl),
+				jwt:      mockshared.NewMockJwt(ctrl),
+				authRepo: mockauth.NewMockAuthRepo(ctrl),
+			}
+			if row.prepare != nil {
+				row.prepare(&f)
+			}
+
+			service := auth.NewAuthService(
+				s.log,
+				f.authRepo,
+				f.crypto,
+				f.jwt,
+			)
+
+			data, err := service.LoginUser(f.tx, row.args)
+			s.Equal(row.err, err)
+			s.Equal(row.expected, data)
 		})
 	}
 }
